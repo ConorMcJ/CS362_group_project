b0091b21839a865cdbbe894a121484401d589120
A. Raheem Faruki <114268594+Abrafar@users.noreply.github.com>
2025-11-12 20:20:54 -0600

    Added Player1Device enum

--- PlayerDevices/Player1Device.ino
+++ PlayerDevices/Player1Device.ino
@@ -7,3 +7,12 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+
+enum PlayerState {
+  WAITING,       // Idle so like we are waiting for command from Simon
+  LED,     // LED pattern replication
+  BUZ,     // Buzzer/joystick pattern replication
+  US,      // Ultrasonic distance matching
+  RECALL   // IR remote input for 3 digit number that we gave at the start after the welcome message
+};
