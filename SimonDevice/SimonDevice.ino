33c085801a4ad17711b5ba38019b210d1f6a792d
A. Raheem Faruki <114268594+Abrafar@users.noreply.github.com>
2025-11-12 20:01:20 -0600

    handlers added

--- SimonDevice/SimonDevice.ino
+++ SimonDevice/SimonDevice.ino
@@ -7,3 +7,20 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+void handleStart(String message) {
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Get Ready!");
+  lcd.setCursor(0,1);
+  lcd.print("Level: " + String(level));
+  gameActive = true;
+}
+
+handleMemory()
+handleLEDPattern()
+handleBuzzerPattern()
+handleUltrasonic()
+handleMemoryRecall()
+handleElimination()
+handleResult()
