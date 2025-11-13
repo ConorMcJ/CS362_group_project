5f5310260b1629ebe33a35e867c2af114c63802f
A. Raheem Faruki <114268594+Abrafar@users.noreply.github.com>
2025-11-12 20:26:27 -0600

    Update SimonDevice.ino

--- SimonDevice/SimonDevice.ino
+++ SimonDevice/SimonDevice.ino
@@ -13,6 +13,56 @@ enum SimonState {
 
 SimonState simonState = MENU;
 
+void loop() {
+
+  // we can add or remove these later
+  handleMenuButtons();
+
+  switch (simonState) {
+
+    case MENU:
+      // so we are just waiting for it to start
+      break;
+
+    case WAITING_START:
+      // maybe show sth like get ready
+      break;
+
+    case SEND_MEMORY_NUM:
+      sendMemoryNumber();
+      simonState = LED_GAME;
+      break;
+
+    case LED_GAME:
+      runLEDGame();
+      break;
+
+    case BUZ_GAME:
+      handleBuzzerPattern();
+      break;
+
+    case US_GAME:
+      handleUltrasonic();
+      break;
+
+    case RECALL_GAME:
+      handleMemoryRecall();
+      break;
+
+    case PROCESS_RESULTS:
+      handleResult();
+      break;
+
+    case GAME_OVER:
+      displayGameOver();
+      break;
+
+    case PAUSED:
+      // Wait for resume
+      break;
+  }
+}
+
 void handleStart(String message) {
   lcd.clear();
   lcd.setCursor(0,0);
@@ -41,7 +91,39 @@ unsigned long lastAction = 0;
 
 
 
+void generateMemoryNumber() {
+  memoryNumber = "";
+  for (int i = 0; i < 3; i++) {
+    memoryNumber += String(random(0, 10));
+  }
+}
 
+void sendMemoryNumber() {
+  generateMemoryNumber();
+
+  lcd.clear();
+  lcd.print("Mem: ");
+  lcd.print(memoryNumber);
+
+  broadcast("S_MEM," + memoryNumber);
+
+  delay(2000);
+}
+
+void generateLEDPattern(int length) {
+  ledPattern = "";
+
+  char colors[4] = {'R', 'G', 'Y', 'B'};
+
+  for (int i = 0; i < length; i++) {
+    int r = random(0, 4);
+    ledPattern += colors[r];
+  }
+}
+
+void sendLEDPattern() {
+  broadcast("S_LED," + ledPattern);
+}
 
 String ledPattern = "";
 void generateLEDPattern(int length) {
